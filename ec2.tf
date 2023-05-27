941fa8da28cf74989befeaea77938de3f6105b3d
Rodriguez <ebaurodrick@gmail.com>
2023-05-26 22:12:10 -0400

    added keypair

--- ec2.tf
+++ ec2.tf
@@ -5,10 +5,14 @@ resource "aws_instance" "backend" {
   availability_zone = var.azs[0]
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.webapp_sg.id]
-
+  key_name = aws_key_pair.deployer.key_name 
   tags = {
     Name = "${var.vpc_name}-${var.backend_instance_name}"
     Env= local.common_tags["Env"]
     Team= local.common_tags["Team"]
   }
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "backend-key"
+  public_key = tls_private_key.private_key.public_key_openssh
 }
--- locals.tf
+++ locals.tf
@@ -5,3 +5,4 @@ locals {
     Team = var.team
  }
 }
+
