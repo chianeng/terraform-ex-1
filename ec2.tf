b497ed26e1ba203b4e04435203b22a5682446f56
Your Name <you@example.com>
2023-06-02 00:11:12 +0000

    update

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "backend" {
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.webapp_sg.id]
   key_name = aws_key_pair.deployer.key_name 
+  iam_instance_profile = "${aws_iam_instance_profile.ec2_profile.name}"
   tags = {
     Name = "${var.vpc_name}-${var.backend_instance_name}"
     Env= local.common_tags["Env"]
